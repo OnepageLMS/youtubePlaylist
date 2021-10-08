1e1496e579440fcb3d2973e044de594e7634af55
ChoHayeong <21800702@handong.edu>
2021-10-08 13:53:44 +0900

    contentList_Stu 수정, url에 대한 classContent가 안보여서 보여지기 위한 작업 중

--- youtubeAPI/src/main/java/com/mycom/myapp/classContent/ClassContentService.java
+++ youtubeAPI/src/main/java/com/mycom/myapp/classContent/ClassContentService.java
@@ -9,8 +9,10 @@ public interface ClassContentService {
 	public int updateContent(ClassContentVO vo);
 	public int updatePublished(ClassContentVO vo);
 	public int deleteContent(ClassContentVO vo); //int id -> ClassContentVO vo수정
+	public int getClassNum(int classID);
 	public ClassContentVO getOneContent(int id);
 	public List<ClassContentVO> getAllClassContent(int classID);
+	public List<ClassContentVO> getFileClassContent(int classID);
 	public List<ClassContentVO> getAllClassContentForCopy(int classID);
 	public int insertCopiedClassContents(List<ClassContentVO> list);
 	public int getDaySeq(ClassContentVO vo);
